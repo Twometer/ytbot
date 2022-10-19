ded027b4ba06ce98b193741f800446e385358b81
Twometer <twometer@outlook.de>
2022-10-19 22:16:14 +0200

    Implement queue mutation commands

--- core/command_handlers.go
+++ core/command_handlers.go
@@ -15,6 +15,7 @@ func init() {
 	RegisterCommand("leave", StopCommand)
 	RegisterCommand("move", MoveCommand)
 	RegisterCommand("clear", ClearCommand)
+	RegisterCommand("remove", RemoveCommand)
 }
 
 func PingCommand(cmd discord.CommandBuffer, client *discord.Client) {
@@ -37,7 +38,10 @@ func PlayCommand(cmd discord.CommandBuffer, client *discord.Client) {
 		return
 	}
 
-	// todo add to playlist
+	botState := GetBotState(cmd.Message)
+	for _, item := range items {
+		botState.Queue = append(botState.Queue, item)
+	}
 
 	if len(items) == 0 {
 		client.EditMessage(statusMsg, ":x: No Results")
@@ -46,6 +50,8 @@ func PlayCommand(cmd discord.CommandBuffer, client *discord.Client) {
 	} else {
 		client.EditMessage(statusMsg, ":white_check_mark: Added **"+strconv.Itoa(len(items))+"** items to queue")
 	}
+
+	// todo trigger playback
 }
 
 func SkipCommand(cmd discord.CommandBuffer, client *discord.Client) {
@@ -57,9 +63,37 @@ func StopCommand(cmd discord.CommandBuffer, client *discord.Client) {
 }
 
 func MoveCommand(cmd discord.CommandBuffer, client *discord.Client) {
+	oldIdx := cmd.GetInt() - 1
+	newIdx := cmd.GetInt() - 1
 
+	botState := GetBotState(cmd.Message)
+
+	newQueue := make([]ytapi.MediaItem, 0)
+	newQueue = append(newQueue, botState.Queue[:newIdx]...)
+	newQueue = append(newQueue, botState.Queue[oldIdx])
+	newQueue = append(newQueue, botState.Queue[newIdx:oldIdx]...)
+	newQueue = append(newQueue, botState.Queue[oldIdx:]...)
+
+	botState.Queue = newQueue
+
+	client.ReplyMessage(cmd.Message, ":white_check_mark: Moved item #"+strconv.Itoa(oldIdx+1)+" to #"+strconv.Itoa(newIdx+1))
 }
 
 func ClearCommand(cmd discord.CommandBuffer, client *discord.Client) {
+	GetBotState(cmd.Message).Queue = nil
+	client.ReplyMessage(cmd.Message, ":white_check_mark: Queue was cleared")
+}
 
+func RemoveCommand(cmd discord.CommandBuffer, client *discord.Client) {
+	index := cmd.GetInt() - 1
+	botState := GetBotState(cmd.Message)
+	if index < 0 || index >= len(botState.Queue) {
+		client.ReplyMessage(cmd.Message, ":x: There is no item with that index")
+		return
+	}
+
+	item := botState.Queue[index]
+
+	botState.Queue = append(botState.Queue[:index], botState.Queue[index+1:]...)
+	client.ReplyMessage(cmd.Message, ":white_check_mark: Item `"+item.Name+"` at position #"+strconv.Itoa(index+1)+" was removed.")
 }
