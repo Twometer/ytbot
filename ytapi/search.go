04a47cf7f40f83f6c838aa6fa2e9f057c2fe7237
Twometer <twometer@outlook.de>
2022-10-20 15:09:10 +0200

    Fix bug with empty results in search

--- ytapi/search.go
+++ ytapi/search.go
@@ -17,8 +17,10 @@ func Search(query string) ([]MediaItem, error) {
 	results := make([]MediaItem, 0)
 
 	_, err = jsonparser.ArrayEach([]byte(ytData), func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-		videoRenderer, _, _, _ := jsonparser.Get(value, "videoRenderer")
-		results = append(results, VideoRendererToMediaItem(videoRenderer, ""))
+		videoRenderer, dataType, _, _ := jsonparser.Get(value, "videoRenderer")
+		if dataType == jsonparser.Object {
+			results = append(results, VideoRendererToMediaItem(videoRenderer, ""))
+		}
 	}, "contents", "twoColumnSearchResultsRenderer", "primaryContents", "sectionListRenderer", "contents", "[0]", "itemSectionRenderer", "contents")
 
 	return results, err
